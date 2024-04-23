19c185a3c2983a0ad5513862de4cdef95d288e39
TSDaniellaEst <marie.estrellado@trajectorservices.com>
2024-04-24 01:59:16 +0800

    changed spacing only

--- danteMRT_testing/tests/navigation.spec.ts
+++ danteMRT_testing/tests/navigation.spec.ts
@@ -20,90 +20,81 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Navigation", () => {
+  test("stations", async ({ page }) => {
+    test.setTimeout(120000);
 
-    test("stations", async ({ page }) => {
-      test.setTimeout(120000)
+    //Navigation
+    await page.getByRole("link", { name: "Stations" }).click();
+    await page.waitForTimeout(3000);
 
-      //Navigation
-      await page.getByRole("link", { name: "Stations" }).click();
-      await page.waitForTimeout(3000);
+    //verification of url
+    await expect(page).toHaveURL(StationsLink);
+    // //Verification of text content within the navigation links
+    expect(await page.textContent("role=columnheader")).toContain(
+      "Station Name"
+    );
+    //visibility & error handling of non-existing link
+    const linkExists = page.locator('a[name="Stations"]');
 
-      //verification of url 
-      await expect(page).toHaveURL(StationsLink);
-      // //Verification of text content within the navigation links
-      expect(await page.textContent("role=columnheader")).toContain("Station Name");
-      //visibility & error handling of non-existing link
-      const linkExists = page.locator('a[name="Stations"]');
-      
-    
-      if (linkExists) {
-        console.log("Exist link");
-      } else {
-        console.log("Does not have any link");
-      }
-     
-    });
-  
-  
-    test("Cards", async ({ page }) => {
-
-      //Navigation
-      await page.getByRole("link", { name: "Cards" }).click();
-      await page.waitForTimeout(3000);
-      //verification of url 
-      await expect(page).toHaveURL(CardsLink);
-      //Verification of text content within the navigation links
-      expect(await page.textContent("role=columnheader")).toContain("Beep Card");
-      //visibility & error handling of non-existing link
-      const linkExists = page.locator('a[name="Cards"]');
-    
-      if (linkExists) {
-        console.log("Exist link");
-      } else {
-        console.log("Does not have any link");
-      }
-  
-    });
-  
-    test("General", async ({ page }) => {
-    
-      //Navigation
-      await page.getByRole("link", { name: "General" }).click();
-      await page.waitForTimeout(3000);
-      //verification of url 
-      await expect(page).toHaveURL(GeneralLink);
-      //Verification of text content within the navigation links
-      expect(await page.textContent("role=button")).toContain("Operational Mode", );
-      //visibility & error handling of non-existing link
-      const linkExists = page.locator('a[name="General"]');
-    
-      if (linkExists) {
-        console.log("Exist link");
-      } else {
-        console.log("Does not have any link");
-      }
-  
-    });
-  
-    test("Log Out", async ({ page }) => {
-      await page.waitForTimeout(3000);
-
-      //Navigation
-      await page.getByRole("link", { name: "Log Out" }).click();
-      //verification of url 
-      await expect(page).toHaveURL(LogOutLink);
-      await page.goto("https://d-mrt-fe.onrender.com/StationManagement#");
-      await expect(page).toHaveURL("https://d-mrt-fe.onrender.com/AdminLogin");
-      //visibility & error handling of non-existing link
-      const linkExists = page.locator('a[name="Log Out"]');
-    
-      if (linkExists) {
-        console.log("Exist link");
-      } else {
-        console.log("Does not have any link");
-      }
-    });
-
-  
+    if (linkExists) {
+      console.log("Exist link");
+    } else {
+      console.log("Does not have any link");
+    }
   });
-  
+
+  test("Cards", async ({ page }) => {
+    //Navigation
+    await page.getByRole("link", { name: "Cards" }).click();
+    await page.waitForTimeout(3000);
+    //verification of url
+    await expect(page).toHaveURL(CardsLink);
+    //Verification of text content within the navigation links
+    expect(await page.textContent("role=columnheader")).toContain("Beep Card");
+    //visibility & error handling of non-existing link
+    const linkExists = page.locator('a[name="Cards"]');
+
+    if (linkExists) {
+      console.log("Exist link");
+    } else {
+      console.log("Does not have any link");
+    }
+  });
+
+  test("General", async ({ page }) => {
+    //Navigation
+    await page.getByRole("link", { name: "General" }).click();
+    await page.waitForTimeout(3000);
+    //verification of url
+    await expect(page).toHaveURL(GeneralLink);
+    //Verification of text content within the navigation links
+    expect(await page.textContent("role=button")).toContain("Operational Mode");
+    //visibility & error handling of non-existing link
+    const linkExists = page.locator('a[name="General"]');
+
+    if (linkExists) {
+      console.log("Exist link");
+    } else {
+      console.log("Does not have any link");
+    }
+  });
+
+  test("Log Out", async ({ page }) => {
+    await page.waitForTimeout(3000);
+
+    //Navigation
+    await page.getByRole("link", { name: "Log Out" }).click();
+    //verification of url
+    await expect(page).toHaveURL(LogOutLink);
+    await page.goto("https://d-mrt-fe.onrender.com/StationManagement#");
+    await expect(page).toHaveURL("https://d-mrt-fe.onrender.com/AdminLogin");
+    //visibility & error handling of non-existing link
+    const linkExists = page.locator('a[name="Log Out"]');
+
+    if (linkExists) {
+      console.log("Exist link");
+    } else {
+      console.log("Does not have any link");
+    }
+  });
+});
