c9beedd409b9d4a0b53fef732b365e017dc7d109
RaizaMondina <167475394+RaizaMondina@users.noreply.github.com>
2024-04-23 22:30:47 +0800

    Update navigation.spec.ts

--- danteMRT_testing/tests/navigation.spec.ts
+++ danteMRT_testing/tests/navigation.spec.ts
@@ -24,25 +24,15 @@ test.describe("Navigation", () => {
       await page.getByRole("textbox", { name: "******************" }).click();
       await page.getByRole("textbox", { name: "******************" }).fill("meow");
       await page.getByRole("button", { name: "Sign In" }).click();
-      await page.waitForTimeout(30000);
+      await page.waitForTimeout(3000);
       //Navigation
       await page.getByRole("link", { name: "Stations" }).click();
-      await page.waitForTimeout(30000);
+      await page.waitForTimeout(3000);
 
       //verification of url 
       await expect(page).toHaveURL(StationsLink);
-      expect(await page.goto('https://d-mrt-fe.onrender.com/StationManagement#'));
-      await page.goto('https://d-mrt-fe.onrender.com/AdminLogin');
-      await page.getByTestId('Admin Link').click({ button: 'right' });
-      await page.waitForTimeout(30000);
-      //Verification of text content within the navigation links
-      //expect(await page.textContent("role=columnheader")).toContain("Station Name");
-      
-      await page.waitForSelector("columnheader", { timeout: 120000  }); // Wait for the selector to appear
-      const textContent = await page.textContent("role=columnheader"); // Get the text content
-      expect(textContent).toContain("Station Name"); // Check if the text content contains "Station Name"
-      
-      
+      // //Verification of text content within the navigation links
+      expect(await page.textContent("role=columnheader")).toContain("Station Name");
       //visibility & error handling of non-existing link
       const linkExists = page.locator('a[name="Stations"]');
       
